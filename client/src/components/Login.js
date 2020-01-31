f6e05b9156b627db654905c4d9db87c1a3e75bbf
jDanielDetes <jdanieldetes@gmail.com>
2020-01-31 12:03:32 -0500

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../axiosWithAuth'
 
-const Login = () => {
+
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [LoginForm,setLoginForm] =useState({
+    username:'',
+    password:''
+  })
+ 
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        axiosWithAuth().post('/login', LoginForm)
+        .then(res=>{
+          console.log(res)
+          localStorage.setItem('token',res.data.payload)
+          props.history.push('/Private')
+        })
+        .catch(err =>{console.log(err)})
+
+      }}>
+        <input 
+        name="username"
+        type="text"
+        placeholder="username"
+        value={LoginForm.username}
+        onChange={e=>{
+          setLoginForm({
+            ...LoginForm,
+            [e.target.name]:e.target.value
+          })
+        }}
+        />
+        <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={LoginForm.password}
+        onChange={e=>{
+          setLoginForm({
+            ...LoginForm,
+            [e.target.name]:e.target.value
+          })
+        }}
+        />
+        <button type='submit'>Login!</button>
+        </form>
     </>
   );
 };
